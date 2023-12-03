21742dada1a7afc39728ae2b296ce5ca79ac82e8
jmoralesss <mail@jmorales.net>
2023-12-02 19:44:37 -0600

    Revert "added tailwind"
    This reverts commit 23b6ab0b6a7dd677ebf92d6173a1561db2d534a4.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
-import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://forloop.dev',
-  integrations: [mdx(), sitemap(), tailwind()]
+	site: 'https://forloop.dev',
+	integrations: [mdx(), sitemap()],
 });
