436dcd0db16260fb271eac488a9770a8ee8702fa
jmoralesss <mail@jmorales.net>
2023-12-05 18:39:54 -0600

    added back tailwind

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'forloop';
-export const SITE_DESCRIPTION = 'Curation of software articles, vidoes, and whitepapers.';
+export const SITE_TITLE = "forloop";
+export const SITE_DESCRIPTION =
+  "Explore the world of software development with forloop. Discover a curated collection of insightful articles, valuable links, and engaging videos covering a wide range of programming topics. Stay updated on the latest trends, best practices, and cutting-edge technologies in the software industry. Whether you're a seasoned developer or a coding enthusiast, ForLoop is your go-to resource for valuable content that fuels your passion for software. Dive into a community-driven platform that brings together expertise, inspiration, and innovation in the ever-evolving realm of programming. Join ForLoop and embark on a journey of continuous learning and discovery in the dynamic world of software development.";
