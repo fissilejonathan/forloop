30b2191725c3ad6bac2195897bca44fd1975e12f
jmoralesss <mail@jmorales.net>
2023-12-07 18:49:03 -0600

    added astro-robots-txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://forloop.dev',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://forloop.dev",
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt()],
 });
