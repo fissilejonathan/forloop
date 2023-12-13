c257c32d9401dda1c8cfcd567e9b18a24453bda5
jmoralesss <mail@jmorales.net>
2023-12-12 20:24:21 -0600

    fixed rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,29 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection("blog");
+  const items = posts.map((post) => {
+    let item = {
+      ...post.data,
+    };
+
+    if (post.data.type !== "link") {
+      item.link = `/blog/${post.slug}/`;
+    } else {
+      item.link = post.data.url;
+    }
+
+    item.categories = post.data.tags;
+
+    return item;
+  });
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: items,
+  });
 }
